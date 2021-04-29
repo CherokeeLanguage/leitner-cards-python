92ec07abd05a6c5cb7e3f3d671482d7d40309e72
Michael Conrad <m.conrad.202@gmail.com>
2021-04-28 23:52:35 -0400

    abstract mutable sequence for deck class

---
+++ cherokee/language/leitner/LeitnerCardData.py
@@ -0,0 +1,9 @@
+class LeitnerCardData:
+    def __init__(self):
+        pass
+
+    def id(self) -> str:
+        return None
+
+    def sort_key(self) -> str:
+        return None
--- cherokee/language/leitner/LeitnerDeck.py
+++ cherokee/language/leitner/LeitnerDeck.py
@@ -1,9 +1,69 @@
+from abc import abstractmethod
+from collections import MutableSequence
+from typing import Iterable
+from typing import overload
+
 from .LeitnerCard import LeitnerCard
 
 
-class LeitnerDeck(list):
+class LeitnerDeck(MutableSequence[LeitnerCard]):
+
+    def __set_deck(self, value: LeitnerCard) -> None:
+        if value:
+            if value.deck:
+                value.deck.remove(value)
+            value.deck = self
+
+    def insert(self, index: int, value: LeitnerCard) -> None:
+        self.__set_deck(value)
+        self.__list.insert(index, value)
+
+    @overload
+    @abstractmethod
+    def __getitem__(self, i: int) -> LeitnerCard:
+        ...
+
+    @overload
+    @abstractmethod
+    def __getitem__(self, s: slice) -> MutableSequence[LeitnerCard]:
+        ...
+
+    def __getitem__(self, i: int) -> LeitnerCard:
+        return self.__list.__getitem__(i)
+
+    @overload
+    @abstractmethod
+    def __setitem__(self, i: int, o: LeitnerCard) -> None:
+        ...
+
+    @overload
+    @abstractmethod
+    def __setitem__(self, s: slice, o: Iterable[LeitnerCard]) -> None:
+        ...
+
+    def __setitem__(self, i: int, o: LeitnerCard) -> None:
+        self.__set_deck(o)
+        self.__list.__setitem__(i, o)
+
+    @overload
+    @abstractmethod
+    def __delitem__(self, i: int) -> None:
+        ...
+
+    @overload
+    @abstractmethod
+    def __delitem__(self, i: slice) -> None:
+        ...
+
+    def __delitem__(self, i: int) -> None:
+        item = self.__getitem__(i)
+        if item:
+            item.deck = None
+        self.__list.__delitem__(i)
+
+    def __len__(self) -> int:
+        return self.__list.__len__()
 
     def __init__(self):
-        super().__init__()
-        self.cards: list[LeitnerCard] = []
-
+        super(LeitnerDeck, self).__init__()
+        self.__list: list[LeitnerCard] = list()
