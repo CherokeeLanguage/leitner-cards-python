0d5edf1004d9988b40fae2395a7308c294b1e646
Michael Conrad <m.conrad.202@gmail.com>
2021-04-30 23:07:33 -0400

    initial setup with Config

--- setup.py
+++ setup.py
@@ -9,18 +9,18 @@ README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setuptools.setup(
-    name="leitner_cards",
-    version="2021.04.30.01",
-    description="Basic Leitner Cards Implementation",
-    long_description=README,
-    long_description_content_type="text/markdown",
-    author="Michael Conrad",
-    author_email="m.conrad.202@gmail.com",
-    license="MIT",
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python"
-    ],
-    packages=setuptools.find_packages(),
-    python_requires=">=3.7"
+        name="leitner_cards",
+        version="2021.04.30.01",
+        description="Basic Leitner Cards Implementation",
+        long_description=README,
+        long_description_content_type="text/markdown",
+        author="Michael Conrad",
+        author_email="m.conrad.202@gmail.com",
+        license="MIT",
+        classifiers=[
+                "License :: OSI Approved :: MIT License",
+                "Programming Language :: Python"
+        ],
+        packages=setuptools.find_packages(),
+        python_requires=">=3.7"
 )
